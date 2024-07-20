322428d5c20b75e43c9ab5feae38add330ac70d8
mmoya064 <mmoya064@uottawa.ca>
2024-07-19 21:08:13 -0400

    quiz

---
+++ react-/src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "Out of the following options, which most closely describes the purpose/occasion you will be using your outfits for?",
+		options: ["Professional/corporate setting", "Party/special celebration", "Day-to-day/casual", "mix of any of the previous options"],
+		answer: "Day-to-day/casual",
+	},
+	{
+		id: 2,
+		question: "Out of the following 'style key-words', does any one of them draw your attention?",
+		options: ["minimalistic", "chic", "whimsical", "mix of any of the previous options"],
+		answer: "chic",
+	},
+	{
+		id: 3,
+		question: "What is the capital of India?",
+		options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+		answer: "Delhi"
+	},
+	{
+		id: 4,
+		question: "What is the capital of Uttarakhad?",
+		options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
+		answer: "Dehradun"
+	},
+	{
+		id: 5,
+		question: "What is capital of Uttar Pradesh?",
+		options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+		answer: "Lucknow"
+	},
+]
+
+export default qBank;
