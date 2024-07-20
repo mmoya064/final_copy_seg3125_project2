605eba84c831d25e21898aac07f51d153d8e7f77
mmoya064 <mmoya064@uottawa.ca>
2024-07-20 12:31:00 -0400

    added "Return" button to the quiz

--- react-/src/index.js
+++ react-/src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const homepage = () => {
+  window.location.href = 'https://mmoya064.github.io/final_copy_seg3125_project2/'; // Replace with the correct path to your HTML file
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+
+    <div type = ""></div>
+    <div>
+      <button type = "button" onClick = {homepage}> RETURN TO HOMEPAGE</button>
+    </div>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
